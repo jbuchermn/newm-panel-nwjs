2dcbc1722d4fa7d0097d53dd8882109ec8e2aa0b
Jonas Bucher <j.bucher.mn@gmail.com>
2021-03-27 10:14:03 +0100

    Update install_unsafe.sh

--- install_unsafe.sh
+++ install_unsafe.sh
@@ -2,5 +2,5 @@
 npm run build-lock
 npm run build-notifiers
 npm run build-launcher
-rm -rf /usr/lib/node_modules/newm-panel
-cp -r . /usr/lib/node_modules/newm-panel
+rm -rf /usr/lib/node_modules/newm-panel-nwjs
+cp -r . /usr/lib/node_modules/newm-panel-nwjs
